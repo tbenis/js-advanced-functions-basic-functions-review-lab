16f9c33240719100b51b777cdc948ddac890d84f
Benis Tambe <will.jenifer@yahoo.com>
2021-08-14 16:37:27 -0400

    commit tree 53b730d188fcf495e022a8ef500da28d99b4e4e3

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+function saturdayFun (activity = "roller-skate" ) {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork =  function (activity = "go to the office" ) {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (param1 = "*"){
+    return function (param2  = "special"){
+        return `You are ${param1}${param2}${param1}!`;
+    }
+}
+
+const Calculator = {
+    add: function(a,b){
+        return a+b;
+    },
+    subtract: function(a,b){
+        return a-b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+    divide: function(a,b){
+        return a/b;
+    }
+}
+
+function actionApplyer(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+    return a
+  }
+  
